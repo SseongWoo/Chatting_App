68d9be59025aa17457bce46f09abbcae8269ba31
SseongWoo <csw10211@gmail.com>
2024-05-04 02:35:40 +0900

    First Commit

---
+++ lib/login/account_find_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AccountFindScreen extends StatefulWidget {
+  const AccountFindScreen({super.key});
+
+  @override
+  State<AccountFindScreen> createState() => _AccountFindScreenState();
+}
+
+class _AccountFindScreenState extends State<AccountFindScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Center(child: Text("AccountFindScreen"));
+  }
+}
